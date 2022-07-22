4f4005878b85fd895663bdb720f9a45478cad442
REghZy <kettlesimzulator420@gmail.com>
2022-07-22 15:40:38 +0100

    Added a "replace instruction" window. Only works on local variable instructions for the moment. Also added a dark theme :)

---
+++ BCEdit180/TypeEditor/DescriptorEditorViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JavaAsm;
+using REghZy.MVVM.ViewModels;
+
+namespace BCEdit180.TypeEditor {
+    public class DescriptorEditorViewModel : BaseViewModel {
+        public TypeDescriptor Descriptor { get; }
+
+        private bool selectedByte;
+        private bool selectedShort;
+        private bool selectedInt;
+        private bool selectedLong;
+        private bool selectedFloat;
+        private bool selectedDouble;
+        private bool selectedChar;
+        private bool selectedBool;
+        private bool selectedVoid;
+        private bool selectedObject;
+
+        public DescriptorEditorViewModel(TypeDescriptor descriptor) {
+            this.Descriptor = descriptor;
+        }
+    }
+}
