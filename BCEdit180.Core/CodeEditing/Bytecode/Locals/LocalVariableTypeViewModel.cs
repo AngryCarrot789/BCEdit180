21c64110f13509150189871f83452b84b299e9b8
REghZy <kettlesimzulator420@gmail.com>
2022-07-31 13:09:19 +0100

    Elements from the LVT and LVTT can be added/removed or cleared

---
+++ BCEdit180.Core/CodeEditing/Bytecode/Locals/LocalVariableTypeViewModel.cs
@@ -0,0 +1,37 @@
+﻿using JavaAsm;
+using REghZy.MVVM.ViewModels;
+
+namespace BCEdit180.Core.CodeEditing.Bytecode.Locals {
+    public class LocalVariableViewModel : BaseViewModel {
+        private ushort startPc;
+        private ushort length;
+        private string variableName;
+        private TypeDescriptor descriptor;
+        private ushort index;
+
+        public ushort StartPC {
+            get => this.startPc;
+            set => RaisePropertyChanged(ref this.startPc, value);
+        }
+
+        public ushort Length {
+            get => this.length;
+            set => RaisePropertyChanged(ref this.length, value);
+        }
+
+        public string VariableName {
+            get => this.variableName;
+            set => RaisePropertyChanged(ref this.variableName, value);
+        }
+
+        public TypeDescriptor Descriptor {
+            get => this.descriptor;
+            set => RaisePropertyChanged(ref this.descriptor, value);
+        }
+
+        public ushort Index {
+            get => this.index;
+            set => RaisePropertyChanged(ref this.index, value);
+        }
+    }
+}
