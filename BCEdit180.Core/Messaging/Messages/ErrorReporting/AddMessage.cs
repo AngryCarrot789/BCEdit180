b7cec96e6c6f396d92d3b187dcdc5928334ee2fa
REghZy <kettlesimzulator420@gmail.com>
2022-07-31 10:21:54 +0100

    Fixed local variable table, where duplicate entry indices are lost and can corrupt classfile. Fixed by replacing dictionary<int, entry> with a list of entries instead

---
+++ BCEdit180.Core/Messaging/Messages/ErrorReporting/AddMessage.cs
@@ -0,0 +1,5 @@
+namespace BCEdit180.Core.Messaging.Messages.ErrorReporting {
+    public class AddMessage {
+        public string Message { get; set; }
+    }
+}
---
+++ BCEdit180.Core/Messaging/Messages/ErrorReporting/ClearMessages.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BCEdit180.Core.Messaging.Messages.ErrorReporting {
+    class ClearMessages {
+    }
+}
